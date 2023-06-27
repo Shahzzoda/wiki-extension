e6effabeb47cf470f43e8cb0fb911a95d042e817
Shahzoda Davlatova <shahzoda@Shahzodas-MacBook-Pro.local>
2023-06-26 23:35:11 -0700

    checks for dupes.

--- scripts/content.js
+++ scripts/content.js
@@ -1,35 +1,60 @@
+// chrome.storage.local.clear();
 // On each page load, content.js scripts get injected
-// So here, we just want to grab all the right information 
-// and send it to background script. 
+// So here, we just want to store all the node information on visit
+// and store a link information when they click anything on the site. 
+// data: {
+//   nodes: [
+//     {id: "url", desc: "short desc", label: "title"}
+//   ],
+//   links: [
+//     { source: "url", target: "url"}
+//   ],
+// exists: Set("url1", "url2")
+// }
+const title = document.getElementById('firstHeading').children[0].innerHTML;        
+const desc = document.getElementsByClassName('shortdescription')[0].innerText;
+var currentPageUrl = window.location.href;
+const node = {
+  label: title,
+  desc: desc,
+  id: currentPageUrl,
+};
+
+chrome.storage.local.get('data', (result) => {  
+  if (Object.keys(result).length == 0) {
+    const storage = {
+      'data': {
+        'nodes': [node],
+        'links': [],
+        'exists': [currentPageUrl],
+      }
+    };
+    chrome.storage.local.set(storage);
+  } else {
+    let existingNodes = new Set(result['data']['exists']);
+    if (!existingNodes.has(currentPageUrl)) {
+      result['data']['nodes'].push(node);
+      result['data']['exists'].push(currentPageUrl)
+      chrome.storage.local.set(result)
+    };
+  }
+});
+
 
 document.addEventListener("click", handleLinkClick);
-
 function handleLinkClick(event) {
-    if (event.target.tagName === "A") {
-        var currentPageUrl = window.location.href;
-        var targetUrl = event.target.href;
-        const title = document.getElementById('firstHeading').children[0].innerHTML;        
-        const desc = document.getElementsByClassName('shortdescription')[0].innerText;
+  if (event.target.tagName === "A") {
+    var targetUrl = event.target.href;
+    const link = {
+      source: currentPageUrl,
+      target: targetUrl,
+      type: 'licensing',
+    };
 
-        const data = {
-            title: title,
-            desc: desc,
-            current_url: currentPageUrl,
-            target_url: targetUrl
-        };
-        
-        const storageData = {};
-        storageData[currentPageUrl] = data;
-      
-        chrome.storage.local.set(storageData, () => {
-          console.log("Value is set");
-        });
-      
-        chrome.storage.local.get(currentPageUrl, (result) => {
-          console.log(result);
-        });
-    }
+    chrome.storage.local.get('data', (result) => {  
+      console.log(result);
+      result['data']['links'].push(link);
+      chrome.storage.local.set(result)
+    });
+  }
 }
-
-// chrome.tabs.create({ url: chrome.runtime.getURL("newtab.html") });
-
